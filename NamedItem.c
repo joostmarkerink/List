4bc9e4d4a55db6c4e20adb2dafb8d3d684f5ef7b
Joost Markerink <joostmarkerink@users.noreply.github.com>
2017-01-16 16:24:30 +0100

    Update NamedItem.c

--- NamedItem.c
+++ NamedItem.c
@@ -1,30 +1,30 @@
 //
 //  NamedItem.c
-//  List
+
 
 /*
-MIT License
-
-Copyright © 2017 Joost Markerink
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ MIT License
+ 
+ Copyright (c) 2017 Joost Markerink
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
 
 #include "NamedItem.h"
 
@@ -55,5 +55,17 @@ bool ItemMethod_findNamed(Item *itm,void *arg){
 }
 
 Item *List_findNamedItem(List *list,const char *name){ 
-    return List_loop(list, ItemMethod_findNamed,(void *)name); 
+    Enumerator e;
+    List_initializeEnumerator(list, &e);
+    Item *i;
+    const char *a,*b;
+    char aa,bb;
+    while((i=next(e))){
+        a=((NamedItem *)i)->name;
+        b=name;
+        aa=0;bb=0;
+        while((aa=*a++) && (bb=*b++)) if(aa!=bb) break;
+        if(aa==bb) return i;
+    }
+    return NULL; 
 }
