e0ed98d4df420c27fbf4481c75c991e22869e636
Joost Markerink <joostmarkerink@users.noreply.github.com>
2017-01-16 16:42:58 +0100

    Update List.h

--- List.h
+++ List.h
@@ -43,7 +43,6 @@ struct _Item{
     Item *previous,*next;
     void *data;
 };
-
 struct _Iteration{
     Item *item;
     Item *(*next)(Iteration *);
@@ -51,15 +50,15 @@ struct _Iteration{
 
 #define iterate(e_n_u_m) e.next(&e_n_u_m)
 
-List *  createList          (void);                                         //create an empty list 
-void    destroyList         (List *);
+List *  createList          (void);                                     //create an empty list 
+void    destroyList         (List *);                                   //destroy list and all its items (does not touch the data)
 
-Item *  createItem          (size_t);                                //create an empty item, 0 size refers to sizeof(Item)
+Item *  createItem          (size_t);                                   //create an empty item, 0 size refers to sizeof(Item)
 
-Item *  List_append         (List *,Item *);                                //append item at the end of the list
-void    List_remove         (List *,Item *);                                //detach an item from the list (use free(item) to destroy it)
-off_t   List_getIndex       (List *,Item *);                                //returns the offset of item or -1 if it is not in the list
-void    List_insert         (List *,Item *toAdd,Item *point,int after);    //insert an item before or after (after = 0/1) an existing item in the list
+Item *  List_append         (List *,Item *);                            //append item at the end of the list
+void    List_remove         (List *,Item *);                            //detach an item from the list (use free(item) to destroy it)
+off_t   List_getIndex       (List *,Item *);                            //returns the offset of item or -1 if it is not in the list
+void    List_insert         (List *,Item *toAdd,Item *point,int after); //insert an item before or after (after = 0/1) an existing item in the list
 
 Item *  List_findItem       (List *,const void *);
 void    List_gatherItems    (List *,Item **     );                      //puts all items in the Items buffer ( Item *itemsBuffer[list->length]; )
