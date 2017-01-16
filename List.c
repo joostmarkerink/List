4bd613fe92422486658116c7f5ee91ae26148ad5
Joost Markerink <joostmarkerink@users.noreply.github.com>
2017-01-16 17:43:46 +0100

    Update List.c

--- List.c
+++ List.c
@@ -1,5 +1,7 @@
 //
-//  List.c
+//  NamedItem.c
+
+
 /*
  MIT License
  
@@ -24,145 +26,46 @@
  SOFTWARE.
  */
 
-#include "List.h"
+#include "NamedItem.h"
 
-List *List_create(){
-    List *l=(List *)malloc(sizeof(List));
-    l->first=l->last=NULL;
-    l->length=0;
-    return l;
+typedef struct _NamedItem NamedItem;
+
+struct _NamedItem{
+    Item item;
+    char name[64];
+};
+
+
+Item *NamedItem_create(const char *name){
+    Item *item=Item_create(sizeof(NamedItem));
+    char c;
+    char *target=((NamedItem *)item)->name;
+    while((c=*name++)) *target++=c;
+    *target++=0;
+    return item;
 }
 
 
-
-void List_destroy(List *list){
-    Iteration e;
-    List_beginIteration(list,&e);
-    Item *itm;
-    while((itm = iterate(e)))
-        free(itm);
-
-    free(list);
+bool ItemMethod_findNamed(Item *itm,void *arg){
+    const char *a=((NamedItem *)itm)->name;
+    const char *b=(const char *)arg;
+    char aa=0,bb=0;
+    while((aa=*a++) && (bb=*b++)) if(aa!=bb) break;
+    return aa==bb;
 }
 
-Item *Item_create(size_t itemSize){
-    Item *i=(Item *)malloc(itemSize<sizeof(Item)?sizeof(Item):itemSize);
-    i->data=NULL;
-    i->previous=i->next=NULL;
-    return i;
-}
-
-
-
-Item *List_findItem(List *list,const void *data){
+Item *List_findNamedItem(List *list,const char *name){ 
     Iteration e;
     List_beginIteration(list, &e);
-    Item *item;
-    while((item=iterate(e))) if(item->data==data) return item;
-    return NULL;
-}
-
-
-Item *List_appendItem(List *list,Item *i){
-    i->previous=list->last;
-    i->next=NULL;
-    if(list->last){
-        list->last->next=i;
-    }else{
-        list->last=list->first=i;
+    Item *i;
+    const char *a,*b;
+    char aa,bb;
+    while((i=iterate(e))){
+        a=((NamedItem *)i)->name;
+        b=name;
+        aa=0;bb=0;
+        while((aa=*a++) && (bb=*b++)) if(aa!=bb) break;
+        if(aa==bb) return i;
     }
-    list->last=i;
-    list->length++;
-    return i;
+    return NULL; 
 }
-
-
-void List_remove(List *list,Item *item){
-    
-    if(list->last==item)  list->last=item->previous;
-    if(list->first==item) list->first=item->next;
-    
-    if(item->next) item->next->previous=item->previous;
-    if(item->previous) item->previous->next=item->next;
-    
-    list->length--;
-    item->next=item->previous=NULL;
-}
-
-
-
-
-off_t List_getItemIndex(List *list,Item *item){
-    off_t offset=0;
-    Iteration e;
-    List_beginIteration(list,&e);
-    Item *itm;
-    while((itm = iterate(e))) if(itm==item) return offset;
-    return offset;
-}
-
-
-
-void List_insert(List *list,Item *newItem,Item *point,int after){
-    if(!point) point=after?list->last:list->first;
-
-    if(after){
-        if(point==list->last){
-            List_appendItem(list, newItem);
-            return;
-        }else if(point->next){
-            newItem->next = point->next;
-            point->next->previous=newItem;
-            newItem->previous=point;
-            point->next=newItem;
-        }
-    }else{
-        if(point){
-            if(point->previous){
-                point->previous->next=newItem;
-                newItem->previous=point->previous;
-            }else{
-                if(list->last == list->first) list->last=newItem;
-                list->first=newItem;
-            }
-            point->previous = newItem;
-        }
-        newItem->next=point;
-    }
-    list->length++;
-}
-
-
-void List_gatherItems(List *list,Item **items){
-    Iteration e;
-    List_beginIteration(list, &e);
-    Item *item;
-    while((item=iterate(e)))
-        *items++=item;
-}
-
-
-
-Item *Iteration_nextItem(Iteration *e){
-    Item *res=e->item;
-    if(res) e->item=res->next;
-    return res;
-}
-
-void List_beginIteration(List *list,Iteration *e){
-    e->item=list->first;
-    e->next=Iteration_nextItem;
-}
-
-Item *Iteration_reverseNextItem(Iteration *i){
-    Item *res=i->item;
-    if(res) i->item=res->previous;
-    return res;
-}
-
-void List_beginReversedIteration(List *list,Iteration *e){
-    e->item=list->last;
-    e->next=Iteration_reverseNextItem;
-}
-
-
